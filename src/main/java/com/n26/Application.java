8a78f9058617659572c8e05940fbb6eb4025d41c
Denis Pozo <denispzpd@gmail.com>
2019-06-12 15:56:36 +0200

    add all base packages to the scanned path to fix not found services error

--- src/main/java/com/n26/Application.java
+++ src/main/java/com/n26/Application.java
@@ -3,7 +3,7 @@ package com.n26;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.n26.service"})
+@SpringBootApplication(scanBasePackages = {"com.n26.*"})
 public class Application {
 
     public static void main(String... args) {
