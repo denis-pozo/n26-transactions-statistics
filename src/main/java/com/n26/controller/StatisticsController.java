51470dafb5d90160de0169efaefc9d727cc201a7
Denis Pozo <denispzpd@gmail.com>
2019-06-12 15:58:10 +0200

    small fixes in the controllers

--- src/main/java/com/n26/controller/StatisticsController.java
+++ src/main/java/com/n26/controller/StatisticsController.java
@@ -3,8 +3,11 @@ package com.n26.controller;
 import com.n26.domain.Statistics;
 import com.n26.service.StatisticsService;
 import com.n26.service.TransactionService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RequestMapping("/statistics")
 @RestController
@@ -16,10 +19,12 @@ public class StatisticsController {
     @Autowired
     StatisticsService statisticsService;
 
-    // TODO return statistics as body
     @GetMapping
-    public void getStatistics() {
-        Statistics stats = statisticsService.computeStatistics(transactionService.getAllTransactions());
+    @ApiOperation(value = "Display transaction statistics of the last 60 seconds.")
+    public Statistics getStatistics() {
+//        Statistics stats = statisticsService.computeStatistics(transactionService.getAllTransactions());
+        Statistics stats = new Statistics(BigDecimal.valueOf(234.53), BigDecimal.valueOf(234.55), BigDecimal.valueOf(325.3), BigDecimal.valueOf(12.0), 3);
+        return stats;
     }
 
 }
--- src/main/java/com/n26/controller/TransactionController.java
+++ src/main/java/com/n26/controller/TransactionController.java
@@ -3,20 +3,23 @@ package com.n26.controller;
 import com.n26.domain.Transaction;
 import com.n26.domain.TransactionRequest;
 import com.n26.service.TransactionService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
 import java.time.Instant;
+import java.time.format.DateTimeParseException;
 
 @RequestMapping("/transactions")
 @RestController
 public class TransactionController {
 
     private static final HttpStatus SUCCESS = HttpStatus.CREATED;
-    private static final HttpStatus DEPRECATED_TRANSACTION = HttpStatus.NO_CONTENT;
+    private static final HttpStatus UNPARSEABLE_TRANSACTION = HttpStatus.NO_CONTENT;
     private static final HttpStatus INVALID_JSON = HttpStatus.BAD_REQUEST;
     private static final HttpStatus PARSING_ERROR = HttpStatus.UNPROCESSABLE_ENTITY;
 
@@ -25,26 +28,32 @@ public class TransactionController {
 
     // TODO Detect when it is an invalid JSON
     @PostMapping
+    @ApiOperation(value = "Add transaction to the transactions list, if it is dated within last 60 secs.")
     public ResponseEntity addTransaction(@RequestBody TransactionRequest request) {
-        BigDecimal amount = request.getAmount();
-        String timestamp = request.getTimestamp();
 
         try {
-            Transaction transaction = new Transaction(amount, Instant.parse(timestamp));
-            transactionService.addTransaction(transaction);
+            BigDecimal amount = request.getAmount();
+            Instant timestamp = Instant.parse(request.getTimestamp());
+            Transaction transaction = new Transaction(amount, timestamp);
+            if(transactionService.addTransaction(transaction)) {
+                return new ResponseEntity(UNPARSEABLE_TRANSACTION);
+            }
         } catch (NullPointerException npe ) {
             return new ResponseEntity(INVALID_JSON);
         } catch (IllegalArgumentException iae) {
-            return new ResponseEntity(DEPRECATED_TRANSACTION);
+            return new ResponseEntity(UNPARSEABLE_TRANSACTION);
+        } catch (DateTimeParseException pe) {
+            return new ResponseEntity(PARSING_ERROR);
         }
 
         return new ResponseEntity(SUCCESS);
     }
 
     @DeleteMapping
+    @ApiOperation(value = "Delete current transactions")
     public ResponseEntity deleteAllTransactions() {
         if (transactionService.deleteAllTransactions()) {
-            return new ResponseEntity(SUCCESS);
+            return new ResponseEntity(UNPARSEABLE_TRANSACTION);
         }
 
         return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
