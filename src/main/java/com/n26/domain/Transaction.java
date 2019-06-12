eb35ff171b384313c8bcb626e51471adcef5e40a
Denis Pozo <denispzpd@gmail.de>
2019-06-12 03:57:08 +0200

    add argument checking tests for transaction

--- src/main/java/com/n26/domain/Transaction.java
+++ src/main/java/com/n26/domain/Transaction.java
@@ -3,12 +3,30 @@ package com.n26.domain;
 import lombok.Value;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Value
 public class Transaction {
 
     BigDecimal amount;
-    LocalDateTime timestamp;
+    Instant timestamp;
+
+    public Transaction(final BigDecimal amount, final Instant timestamp){
+        if(amount == null || timestamp == null) {
+            throw new NullPointerException("Input parameters cannot be null");
+        }
+
+        /* Assumption: No delay from the POST request with the transaction until the creation of
+         * the instance. Theoretically a faked post request with a date in the future (~ delay)
+         * will pass this argument checker. However, at this level is almost impossible to correct
+         * the effect of the delay. */
+        if (timestamp.isAfter(Instant.now())) {
+            throw new IllegalArgumentException (
+                "Illegal timestamp: Transactions cannot be done in the future (yet)");
+        }
+
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
 
 }
--- src/test/java/com/n26/domain/TransactionTest.java
+++ src/test/java/com/n26/domain/TransactionTest.java
@@ -1,22 +1,48 @@
 package com.n26.domain;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jni.Time;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+@Slf4j
 public class TransactionTest {
 
-    // TODO Check validity of the passed in parameters
-    // 1. amount
-    @Test(expected = Exception.class)
-    public void constructorShouldThrowExceptionWhen_AmountIsNull() throws Exception {
-        throw new Exception();
+    private static final BigDecimal amount = BigDecimal.valueOf(12.3347);
+    private static final Instant timestamp = Instant.now();
+
+    @Test
+    public void constructorShouldCreateAnInstanceCorrectly() {
+        log.info("Test: constructor creates an instance when input parameters are correct");
+        Transaction transaction = new Transaction( amount, timestamp);
+        assertNotNull(transaction);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void constructorShouldThrowExceptionWhen_AmountIsNull() throws Exception {
+        log.info("Test: constructor throws exception when input argument 'amount' is null");
+        Transaction transaction = new Transaction(null, timestamp);
+    }
 
-    // 2. timestamp
     @Test(expected = Exception.class)
     public void constructorShouldThrowExceptionWhen_TimestampIsNull() throws Exception {
-        throw new Exception();
+        log.info("Test: constructor throws exception when input argument 'timestamp' is null");
+        Transaction transaction = new Transaction(amount, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorShouldThrowExceptionWhen_TimestampIsInTheFuture() {
+        log.info("Test: constructor throws exception when input argument 'timestamp' is in the future");
+        Instant timestamp = Instant.now().plusMillis(10);
+        Transaction transaction = new Transaction(amount, timestamp);
     }
 }
